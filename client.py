08fa70cf6b8e4c16c6198cb712788e64e0d9973e
Nikhil Sarda <nikhilsarda.iitkgp@gmail.com>
2013-07-12 01:08:38 -0400

    Added hmac

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ name=raw_input("What is your name? ").strip()
 
 Pyro4.config.HMAC_KEY = 'Nikhil'
 #room=Pyro4.Proxy("PYRONAME:room")          # get a Pyro proxy to the greeting object
-room=Pyro4.Proxy("PYRO:obj_6f81576f462c4ff1bdd6e4bedc2a880f@192.168.0.164:60389")
+room=Pyro4.Proxy("PYRO:obj_6f81576f462c4ff1bdd6e4bedc2a880f@192.168.0.164:60389`")
 room.add_me(name)   # call method normally
 remote_uuid = room.add_me('remote')
 print "Users are ", room.get_users()
--- test1.py
+++ test1.py
@@ -1,4 +1,5 @@
 import Pyro4
+Pyro4.config.HMAC_KEY = 'Nikhil'
 nameserver = Pyro4.locateNS(host='192.168.0.6', port=9090)
 nameserver.list()
 uri = nameserver.lookup('room')
