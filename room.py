2cce487b674f63dd4f7462edd558a7b1dbb1c1a9
Nikhil Sarda <nikhilsarda.iitkgp@gmail.com>
2013-07-11 22:14:59 -0400

    Added logging off and inbox

--- room.py
+++ room.py
@@ -3,11 +3,21 @@ import Pyro4
 class Room(object):
     users = []
 
+    inbox = {}
+
     msg = []
 
     def add_me(self, name):
-        self.users.append(name)
-        return "Hello, {0}. Added.\n".format(name)
+        if name not in self.users:
+            self.users.append(name)
+            return "Hello, {0}. Added.\n".format(name)
+        else:
+            return "User already exists!"
+
+    def log_off(self, name):
+        if name in self.users:
+            self.users.remove(name)
+        return "Logged off"
 
     def get_users(self):
         return self.users
@@ -18,6 +28,16 @@ class Room(object):
 
     def get_msg(self):
         return ' '.join(self.msg)
+
+    def send_msg_to_user(self, user, msg):
+        if user in self.users:
+            if not self.inbox.has_key(user):
+                self.inbox[user] = []
+            self.inbox[user].append(msg)
+        return "Sent"
+
+    def get_my_msg(self, name):
+        return self.inbox[name]
 
 room = Room()
 
