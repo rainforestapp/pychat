bec4d3e5de09b75cb3314ef8089f8f4d3af9d740
Russell Smith <russell.smith@ukd1.co.uk>
2013-07-11 17:48:42 -0700

    Fix

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ from pydht import DHT
 host, port = '192.168.0.6', 3000
 dht = DHT(host, port, boot_host='192.168.0.164', 3000)
 
-dht = DHT(host, port, boot_host=host, boot_port=port)
-
 dht["test"] = "Hello world"
 dht["test"]
 # OUT: u'Hello world'
